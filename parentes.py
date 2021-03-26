afe5726a9391bab5f5b093d28f1fc539633833fe
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2021-03-26 10:03:14 +0000

    Alinea c done

--- parentes.py
+++ parentes.py
@@ -1,61 +1,48 @@
 import re
+import operator
 
 
-def toSingular(group):
-    return individualGrupo[group]
+def isPlural(member):
+    return (member[-1] == 's')
 
 
-file = open('processos.xml', 'r')
+def toSingular(plural):
+    singular = re.sub(r'(s )', r' ', plural)
+    return re.sub(r's$', r'', singular)
 
-next(file)
-next(file)
 
-familiaresIndividuais = ["Irmao", "Tio Materno", "Sobrinho Materno", "Tio Paterno",
-                         "Sobrinho Paterno", "Tio Avo Materno", "Tio Avo Paterno",
-                         "Avo Materno", "Avo Paterno", "Bisavo Materno", "Bisavo Paterno",
-                         "Primo", "Primo Paterno", "Primo Materno"]
-
-familiaresGrupos = ["Irmaos", "Primos", "Tios Paternos", "Tios Maternos",
-                    "Sobrinhos Maternos", "Sobrinhos Paternos"]
-
-individualGrupo = {"Irmaos": "Irmao", "Primos": "Primo", "Tios Paternos": "Tio Paterno",
-                   "Tios Maternos": "Tio Materno", "Sobrinhos Maternos": "Sobrinho Materno", "Sobrinhos Paternos": "Sobrinho Paterno"}
-
-familyRE = re.compile(
-    r'(<obs>|\. )(((\w+| )+),({}).)'.format('|'.join(familiaresIndividuais) + '|' + '|'.join(familiaresGrupos)))
+obsRE = re.compile(r'<obs>((.|\n)*?)</obs>')
+familyRE = re.compile(r'(\. )?((.+),(.+))\. Proc')
 
 withFamily = 0
-
 frequencyFamily = {}
 
-for line in file:
-    m = re.findall(familyRE, line.strip())
+with open("processos.xml") as f:
+    conteudo = f.read()
+    obs = re.findall(obsRE, conteudo)
 
-    if m:
+for line in obs:
+    linha = re.sub(r'( |\n)+', r' ', line[0].strip())
+    family = re.findall(familyRE, linha)
+
+    if family:
         withFamily += 1
 
-        for relativeGroup in m:
-            print(relativeGroup)
-            relative = relativeGroup[3]
-            grau = relativeGroup[4]
+        for relativeGroup in family:
+            grau = relativeGroup[3]
+            numFamiliares = 1
 
-            if relative in familiaresGrupos:
-                familiares = relativeGroup[2].split('e')
-                numFamiliares = familiares.size()
-                if relative in frequencyFamily:
-                    frequencyFamily[toSingular(grau)] += numFamiliares
-                else:
-                    frequencyFamily[toSingular(grau)] = numFamiliares
+            if isPlural(grau):
+                relatives = re.split(' e |, ', relativeGroup[2])
+                numFamiliares = len(relatives)
+                grau = toSingular(grau)
 
+            if grau in frequencyFamily:
+                frequencyFamily[grau] += numFamiliares
             else:
-                if relative in frequencyFamily:
-                    frequencyFamily[grau] += 1
-                else:
-                    frequencyFamily[grau] = 1
+                frequencyFamily[grau] = numFamiliares
 
-print("Numero de candidatos com parentes eclásticos: " + withFamily)
+print("Numero de candidatos com parentes eclásticos: " + str(withFamily))
 
 print("Tipo de parentesco mais frequente: " +
-      max(stats.iteritems(), key=operator.itemgetter(1))[0])
-
-file.close()
+      max(frequencyFamily.items(), key=operator.itemgetter(1))[0])
