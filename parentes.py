9283dc70b9a363f8ac0b929cafc0b3f256132ec9
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2021-03-26 01:11:35 +0000

    Avanços alinea c

--- parentes.py
+++ parentes.py
@@ -1,35 +1,61 @@
 import re
 
 
+def toSingular(group):
+    return individualGrupo[group]
+
+
 file = open('processos.xml', 'r')
 
 next(file)
 next(file)
 
+familiaresIndividuais = ["Irmao", "Tio Materno", "Sobrinho Materno", "Tio Paterno",
+                         "Sobrinho Paterno", "Tio Avo Materno", "Tio Avo Paterno",
+                         "Avo Materno", "Avo Paterno", "Bisavo Materno", "Bisavo Paterno",
+                         "Primo", "Primo Paterno", "Primo Materno"]
 
-family = re.compile(r',(\w+( \w+)?)\.')
+familiaresGrupos = ["Irmaos", "Primos", "Tios Paternos", "Tios Maternos",
+                    "Sobrinhos Maternos", "Sobrinhos Paternos"]
+
+individualGrupo = {"Irmaos": "Irmao", "Primos": "Primo", "Tios Paternos": "Tio Paterno",
+                   "Tios Maternos": "Tio Materno", "Sobrinhos Maternos": "Sobrinho Materno", "Sobrinhos Paternos": "Sobrinho Paterno"}
+
+familyRE = re.compile(
+    r'(<obs>|\. )(((\w+| )+),({}).)'.format('|'.join(familiaresIndividuais) + '|' + '|'.join(familiaresGrupos)))
 
 withFamily = 0
 
 frequencyFamily = {}
 
 for line in file:
-
-    m = re.findall(family, line)
+    m = re.findall(familyRE, line.strip())
 
     if m:
         withFamily += 1
-        print(m)
-        # print(m.group(3), m.group(4))
 
-        for relative in m:
-            if relative in frequencyFamily:
-                frequencyFamily[relative] += 1
+        for relativeGroup in m:
+            print(relativeGroup)
+            relative = relativeGroup[3]
+            grau = relativeGroup[4]
+
+            if relative in familiaresGrupos:
+                familiares = relativeGroup[2].split('e')
+                numFamiliares = familiares.size()
+                if relative in frequencyFamily:
+                    frequencyFamily[toSingular(grau)] += numFamiliares
+                else:
+                    frequencyFamily[toSingular(grau)] = numFamiliares
+
             else:
-                frequencyFamily[relative] = 1
+                if relative in frequencyFamily:
+                    frequencyFamily[grau] += 1
+                else:
+                    frequencyFamily[grau] = 1
 
-print(withFamily)
-print(frequencyFamily)
+print("Numero de candidatos com parentes eclásticos: " + withFamily)
 
+print("Tipo de parentesco mais frequente: " +
+      max(stats.iteritems(), key=operator.itemgetter(1))[0])
 
 file.close()
