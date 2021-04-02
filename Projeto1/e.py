2b8201f4ff92449de32315dd543be0694a3c27ea
luissobral4 <luisfilipesobral@gmail.com>
2021-04-02 17:36:53 +0100

    alinea e

--- Projeto1/e.py
+++ Projeto1/e.py
@@ -3,6 +3,12 @@ from graphviz import Digraph
 
 dot = Digraph()
 processes = set()
+idP = 0
+
+def incrementa():
+    global idP
+    idP += 1
+    return str(idP)
 
 while True:
     data = input("Introduzir ano >> ")
@@ -12,8 +18,9 @@ while True:
     except ValueError:
         print("Valor Inválido")
 
+familyRE = re.compile(r'([a-zA-Z, ]+),(Irmao|Irmaos)\.[ ]?Proc\.(\d+)')
 contentRE = re.compile(
-    r'<processo id="(\d+)">(.|\n)*?<data>((\d{4})-\d{2}-\d{2})</data>(.|\n)*?<nome>(.+)</nome>(.|\n)*?<pai>(.+)</pai>(.|\n)*?<mae>(.+)</mae>')
+    r'<processo id="(\d+)">(.|\n)*?<data>((\d{4})-\d{2}-\d{2})</data>(.|\n)*?<nome>(.+)</nome>(.|\n)*?<pai>(.+)</pai>(.|\n)*?<mae>(.+)</mae>(.|\n)*?(<obs>((.|\n)*?)</obs>|<obs/>)')
 
 with open("processos.xml") as f:
     file = f.read()
@@ -25,10 +32,29 @@ for line in content:
         year = int(line[3])
 
         if year == data:
-            dot.node(line[5])
-            dot.node(line[7])
-            dot.node(line[9])
-            dot.edge(line[5], line[7])
-            dot.edge(line[5], line[9])
+            linha = re.sub(r'( |\n)+', r' ', line[12].strip())
+            family = re.findall(familyRE, linha)
+            idfilho = incrementa()
+            idmae = incrementa()
+            idpai = incrementa()
+            dot.node(idfilho,line[5])
+            dot.node(idpai,line[7])
+            dot.node(idmae,line[9])
+            dot.edge(idpai, idfilho)
+            dot.edge(idmae, idfilho)
+
+            for relativeGroup in family:
+                grau = relativeGroup[1]
+                processes.add(relativeGroup[2])
+
+                relatives = re.split(' e| e |, |,', relativeGroup[0])
+
+                for relative in relatives:
+                    idirmao = incrementa()
+                    dot.node(idirmao,relative)
+                    dot.edge(idpai,idirmao)
+                    dot.edge(idmae,idirmao) 
+
+
 
 dot.render('output/graph.gv', view=True)
